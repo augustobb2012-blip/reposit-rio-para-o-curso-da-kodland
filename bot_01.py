05ae131c896eda8746f56d7154aff64f9440f715
augusto <augusto.bb2012@gmail.com>
2026-04-14 16:27:16 -0300

    Update bot_01.py

--- bot_01.py
+++ bot_01.py
@@ -1,38 +1,43 @@
 import discord
 from discord.ext import commands
 
-# Permissões
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Criando o bot
 bot = commands.Bot(command_prefix="$", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Fizemos login como {bot.user}')
 
-# Mantendo o MESMO comportamento do seu on_message
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send(f"Hello! i am {bot.user}!")
-
-    elif message.content.startswith('$bye'):
-        await message.channel.send("🙂")
-
-    elif message.content.startswith('$hoje'):
-        await message.channel.send("ebaa!!! aula de python!!!")
-
-    else:
-        await message.channel.send(message.content)
-
-    # ESSA LINHA É MUITO IMPORTANTE
-    await bot.process_commands(message)
+# Comandos
 
 @bot.command()
-async def joined(ctx, *, member: discord.Member):
-    await ctx.send(f'{member} joined on {member.joined_at}')
+async def hello(ctx):
+    await ctx.send(f"Hello! I am {bot.user}!")
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("Bye!")
+
+@bot.command()
+async def hoje(ctx):
+    await ctx.send("Ebaa!!! Aula de Python!!!")
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    await ctx.send(f'{member} entrou em {member.joined_at}')
+
+# COMANDO DE AJUDA
+@bot.command()
+async def help(ctx):
+    comandos = """
+Comandos do Bot:
+
+$hello -> O bot te cumprimenta  
+$bye -> O bot se despede  
+$hoje -> Mensagem sobre a aula  
+$joined @usuario -> Mostra quando o usuário entrou no servidor  
+$help -> Mostra essa lista de comandos
+"""
+    await ctx.send(comandos)
